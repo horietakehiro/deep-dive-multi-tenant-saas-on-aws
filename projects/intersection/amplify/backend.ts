4ea672b16b55c6f272949449e17cbf026987d701
horie.takehiro <horie.takeihro@outlook.jp>
2025-05-01 13:21:55 +0900

    試行錯誤

--- projects/intersection/amplify/backend.ts
+++ projects/intersection/amplify/backend.ts
@@ -1,4 +1,4 @@
-import { defineBackend, referenceAuth } from "@aws-amplify/backend";
+import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 
