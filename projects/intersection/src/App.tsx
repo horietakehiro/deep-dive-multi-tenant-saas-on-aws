06b76e4a24afa750b84f254f9361dfac97a8a00c
horie.takehiro <horie.takeihro@outlook.jp>
2025-05-01 14:00:37 +0900

    試行錯誤

--- projects/intersection/src/App.tsx
+++ projects/intersection/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { greet } from "../../control-plane/src/share";
+import { greet } from "@/shared";
 
 function App() {
   const [count, setCount] = useState(0);
